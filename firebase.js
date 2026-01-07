d33b0e571b08c4bf399dad99b3fb8dcf36360464
namabei65-png <namabei65@gmail.com>
2026-01-08 02:44:13 +0900

    Update Firebase configuration and imports

--- firebase.js
+++ firebase.js
@@ -1,26 +1,18 @@
 <script type="module">
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const app = initializeApp({
   apiKey: "AIzaSyBO9-mZ8L7G6gehMLVk3AzecCy94OuVamY",
   authDomain: "ultimate-quiz-e42ee.firebaseapp.com",
   databaseURL: "https://ultimate-quiz-e42ee-default-rtdb.firebaseio.com",
-  projectId: "ultimate-quiz-e42ee",
-  storageBucket: "ultimate-quiz-e42ee.firebasestorage.app",
-  messagingSenderId: "252962417614",
-  appId: "1:252962417614:web:37649ddf0ad1e2e997599f",
-  measurementId: "G-0BL3MXZWTG"
-};
+  projectId: "ultimate-quiz-e42ee"
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getDatabase(app);
+</script>
+
 
 </script>
